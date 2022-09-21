90a58fc5b1ecafee9bcde03ba9068738a791e90d
Natalie <hairnatalie7@gmail.com>
2022-09-21 14:47:09 -0600

    2nd commit

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express()
 
 app.use(express.json())
 
+app.use('/', express.static(path.join(__dirname, '/index.html')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+ app.get('/js', (req, res) => {
+   res.sendFile(path.join(__dirname, '/index.js'))
+ })
 app.listen(port, ()=> {
     console.log('Docked at port ' + port)
 })
